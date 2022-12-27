839e809160cab2fc221732fe0b8997962e622e19
Maryna M <markova.maryna@gmail.com>
2022-12-27 18:04:22 +0200

    add basic react structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Dictionary-App
+# DICTIONARY-APP
+
+This **Dictionary-App** is built as a course project on [SheCodes React](https://www.shecodes.io/).
+
+You are welcome to [check it out!]()
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <footer>
+        <a
+          href="https://github.com/MarynaMarkova/Dictionary-App"
+          className="profileLink"
+          target="_blank"
+          rel="noreferrer"
+          title="Github code"
+        >
+          Open-source
+        </a>
+        {" by "}
+        <a
+          href="https://marynamarkova.github.io/"
+          className="profileLink"
+          target="_blank"
+          rel="noreferrer"
+          title="Personal page"
+        >
+          Maryna Markova
+        </a>
+      </footer>
+    </div>
+  );
+}
