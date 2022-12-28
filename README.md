23bfb69e6afaf9471e5cc93459d218514bc0f0e2
Maryna M <markova.maryna@gmail.com>
2022-12-28 16:17:13 +0200

    create search engine

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This **Dictionary-App** is built as a course project on [SheCodes React](https://www.shecodes.io/).
 
-You are welcome to [check it out!]()
+You are welcome to [check it out!](https://gilded-cheesecake-65f930.netlify.app/)
--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        <a
-          href="https://github.com/MarynaMarkova/Dictionary-App"
-          className="profileLink"
-          target="_blank"
-          rel="noreferrer"
-          title="Github code"
-        >
-          Open-source
-        </a>
-        {" by "}
-        <a
-          href="https://marynamarkova.github.io/"
-          className="profileLink"
-          target="_blank"
-          rel="noreferrer"
-          title="Personal page"
-        >
-          Maryna Markova
-        </a>
-      </footer>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          <a
+            href="https://github.com/MarynaMarkova/Dictionary-App"
+            className="profileLink"
+            target="_blank"
+            rel="noreferrer"
+            title="Github code"
+          >
+            Open-source
+          </a>
+          {" by "}
+          <a
+            href="https://marynamarkova.github.io/"
+            className="profileLink"
+            target="_blank"
+            rel="noreferrer"
+            title="Personal page"
+          >
+            Maryna Markova
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
